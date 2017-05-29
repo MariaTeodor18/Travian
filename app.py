b9ca936b06626f5b532ffaededc7032848f0d5e1
MariaTeodor18 <teodorvalentina@yahoo.com>
2017-05-29 07:23:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import signal
 
 app = Flask(__name__)
 
-ControlPin = [11,15,16,18]
+ControlPin = [11,15,38,40]
 
 seq = [ [1,0,0,0],
 	[1,1,0,0],
@@ -18,8 +18,13 @@ seq = [ [1,0,0,0],
 	[0,0,0,1],
 	[1,0,0,1] ]
 
+auto=0
+
 def feed():
-    GPIO.setmode(GPIO.BOARD)
+    global auto
+    if(auto==0):
+        GPIO.setmode(GPIO.BOARD)
+
     for pin in ControlPin:
         GPIO.setup(pin,GPIO.OUT)
         GPIO.output(pin,0)
@@ -30,37 +35,48 @@ def feed():
                 GPIO.output(ControlPin[pin],seq[halfstep][pin])
             time.sleep(0.004)
                     
-    GPIO.cleanup()
+    if(auto==0):                
+        GPIO.cleanup()
 
 def startAutoFeed():
-    TRIG = 22
-    ECHO = 24
+    global auto
+    auto=1
+    GPIO.setmode(GPIO.BOARD)
+    TRIG = 16
+    ECHO = 18
     currentTime = time.time() - 4*3600;
     
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.setup(ECHO,GPIO.IN)
+
+    GPIO.output(TRIG, 0)
+
     while True:
         
-        GPIO.output(TRIG, False)                 #Set TRIG as LOW
-        print "Waitnig For Sensor To Settle"
-        time.sleep(2)                            #Delay of 2 seconds
+        GPIO.output(TRIG, 0)                 #Set TRIG as LOW
+        
+        time.sleep(1)                            #Delay of 2 seconds
 
-        GPIO.output(TRIG, True)                  #Set TRIG as HIGH
+        GPIO.output(TRIG, 1)                  #Set TRIG as HIGH
         time.sleep(0.00001)                      #Delay of 0.00001 seconds
-        GPIO.output(TRIG, False)                 #Set TRIG as LOW
+        GPIO.output(TRIG, 0)                 #Set TRIG as LOW
 
         while GPIO.input(ECHO)==0:               #Check whether the ECHO is LOW
-            pulse_start = time.time()            #Saves the last known time of LOW pulse
+            pulse_start = time.time()              #Saves the last known time of LOW pulse
 
-        while GPIO.input(ECHO)==1 :              #Check whether the ECHO is HIGH
-            pulse_end = time.time()              #Saves the last known time of HIGH pulse
+        while GPIO.input(ECHO)==1 :               #Check whether the ECHO is HIGH
+            pulse_end = time.time()                #Saves the last known time of HIGH pulse
 
         pulse_duration = pulse_end - pulse_start
         distance =pulse_duration * 17150
         distance = round(distance, 2)
 
-        if time.time()-currentTime > 4*3600  and distance > 2 and distance < 20:
+        if time.time()-currentTime > 10  and distance > 2 and distance < 20:
             print 'Feeding Now!'
             feed()
             currentTime=time.time()
+    GPIO.cleanup()
+    auto=0
 
 pid=0
 
@@ -68,15 +84,21 @@ pid=0
 def index():
     global pid
     print pid
+    return render_template('index.html')
+
+@app.route('/stop/')
+def stop():
+    global pid
+    print pid
     if (pid > 0):
-        os.kill(pid,signal.SIGKILL)
+	os.kill(pid,signal.SIGKILL)
     return render_template('index.html')
 
 @app.route('/feednow/')
 def feedNow():
     print 'Feeding Now!'
     feed()
-    return render_template('feedNow.html')
+    return render_template('index.html')
 
 @app.route('/autofeed/')
 def autoFeed():
@@ -86,7 +108,7 @@ def autoFeed():
     if (pid == 0):
         startAutoFeed()
     print pid
-    return render_template('autoFeed.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
